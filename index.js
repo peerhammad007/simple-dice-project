68146f53143c26cc05d2bfb49e9d74e10fe4950c
peerhammad007 <91062013+peerhammad007@users.noreply.github.com>
2021-10-31 18:54:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,19 +24,20 @@ function randomNumber() {
   return randomNumber1;
 }
 
+if(window.performance.navigation.type == 1) {
+  var randNum1 = randomNumber();
+  document.querySelector(".img1").setAttribute("src", src);
+  randomNumber();
+  var randNum2 = randomNumber();
+  document.querySelector(".img2").setAttribute("src", src);
 
-var randNum1 = randomNumber();
-document.querySelector(".img1").setAttribute("src", src);
-randomNumber();
-var randNum2 = randomNumber();
-document.querySelector(".img2").setAttribute("src", src);
-
-if(randNum1 > randNum2) {
-  document.querySelector("h1").innerText = "🚩Player 1 Wins!";
-}
-else if(randNum2 > randNum1) {
-  document.querySelector("h1").innerText = "Player 2 Wins!🚩";
-}
-else {
-  document.querySelector("h1").innerText = "Draw";
+  if(randNum1 > randNum2) {
+    document.querySelector("h1").innerText = "🚩Player 1 Wins!";
+  }
+  else if(randNum2 > randNum1) {
+    document.querySelector("h1").innerText = "Player 2 Wins!🚩";
+  }
+  else {
+    document.querySelector("h1").innerText = "Draw";
+  }
 }
